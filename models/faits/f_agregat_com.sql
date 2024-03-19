48c27f79ac4431d66fb9685b64b2e44d60904cfc
OceMaillot <omaillot@decideom.fr>
2024-03-19 16:41:41 +0100

    update agregat_com

--- models/faits/f_agregat_com.sql
+++ models/faits/f_agregat_com.sql
@@ -1,32 +1,22 @@
-WITH agregat_com AS (
-       SELECT YEAR(DATE_FAC) AS ANNEE
-        , code_commercial
-        , CODE_ETABLISSEMENT  
-        , SUM(objectif) as objectif
-        , SUM(CHIFFRE_AFFAIRE_FAC) as CHIFFRE_AFFAIRE_FAC      
-from (
-    SELECT 
-        FAC.CODE_COMMERCIAL,
-        FAC.DATE_FAC,
-        SUM(FAC.chiffre_affaire_fac) AS chiffre_affaire_fac,
-        CODE_ETABLISSEMENT,
-        OBJ.objectif
-    FROM   
-        {{ ref('f_agregat')}} AS FAC
-    INNER JOIN {{ ref('f_objectif')}} AS OBJ ON OBJ.OBJECTIF = OBJECTIF
-    GROUP BY 
-        COM.CODE_COMMERCIAL,
-        DAT.DATE_FAC,
-        OBJ.objectif,
-        COM.CODE_ETABLISSEMENT
-) AGR
-GROUP BY YEAR(DATE_FAC)
-        , CODE_COMMERCIAL
-        , code_etablissement
+with calc_ca as (
+    select code_commercial
+          ,sum(chiffre_affaire_fac) as CA
+          ,annee 
+    from {{ref('f_agregat')}}
+group by code_commercial
+        ,annee
+),
+calc_obj as (
+    select code_commercial
+          ,sum(OBJECTIF) as objectif
+          ,year(date_fac) as annee 
+    from {{ref('f_objectif')}}
+group by code_commercial
+        ,year(date_fac)
 )
-SELECT * FROM agregat_com
-ORDER BY ANNEE DESC
-
---SELECT CODE_ETABLISSEMENT FROM   {{ ref('f_agregat')}} AS FAC
-
---SELECT * FROM {{ ref('f_objectif')}} 
+    select a.code_commercial
+      ,a.ca
+      ,b.objectif
+      ,a.annee
+    from calc_ca a 
+    inner join calc_obj b on a.code_commercial = b.code_commercial and a.annee=b.annee
